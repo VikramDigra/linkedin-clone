56b7247110a6f2b91aa0868f47840a32a7e0b0dd
Vikram Digra <87901614+VikramDigra@users.noreply.github.com>
2023-02-07 22:42:36 +0530

    b
    h

--- linkedin/src/Header.js
+++ linkedin/src/Header.js
@@ -1,21 +1,21 @@
 import React from 'react'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import SearchIcon from '@mui/icons-material/Search'
+import './Header.css'
 
 function Header() {
   return (
-    <div>
-      {' '}
-      <div className='header'>
-        <div className='header-left'>
-          <div className='header-image'>
-            <LinkedInIcon />
-          </div>
-          <div className='header-search'>
-            <SearchIcon />
-            <input type='text' name='' id='header-input' />
-          </div>
+    <div className='header'>
+      <div className='header-left'>
+        <div className='header-image'>
+          <LinkedInIcon />
         </div>
-        <div className='header-right'></div>
+        <div className='header-search'>
+          <SearchIcon />
+          <input type='text' name='' id='header-input' placeholder='search' />
+        </div>
       </div>
+      <div className='header-right'></div>
     </div>
   )
 }
